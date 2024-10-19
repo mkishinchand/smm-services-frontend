ca58c9794f6c066e78de1392e91ffa9e07a61610
mkishinchand <mohankishinchand.contacto@gmail.com>
2024-10-18 19:27:44 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const API_URL = 'https://smm-services-frontend-fpon-111.vercel.app/'; // Replace with your actual Render backend URL
+const API_URL = 'https://smm-swkt.onrender.com'; // Replace with your actual Render backend URL
 
 function App() {
   const [formData, setFormData] = useState({
